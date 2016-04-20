1d13d62b8640d7fc171568c6785eaa73c9e3e18b
claytongentry <claytongentry2017@u.northwestern.edu>
2016-04-19 19:51:26 -0500

    first set toasted

--- src/models/recipe.py
+++ src/models/recipe.py
@@ -98,58 +98,196 @@ class Recipe(object):
                             'number': '6'
                         },
                     ],
-                    'image':'link/to/image'
+                    'image':'/images/broccoli.png'
                 },
                 {
                     'title': 'Roasted Balsamic Broccoli',
                     'ingredients':[
                         {
-                            'name':'ing 1',
+                            'name':'frozen (or fresh) broccoli florets',
+                            'quantity': '4',
+                            'unit':'cups'
+                        },
+                        {
+                            'name':'balsamic vinegar',
+                            'quantity': '3',
+                            'unit':'tbsps'
+                        },
+                        {
+                            'name':'extra-virgin olive oil',
                             'quantity': '2',
-                            'unit':'oz'
+                            'unit':'tbsps'
+                        },
+                        {
+                            'name':'sea salt',
+                            'quantity': '1',
+                            'unit':'tsp'
+                        },
+                        {
+                            'name':'black pepper',
+                            'quantity': '1',
+                            'unit':'tsp'
+                        },
+                        {
+                            'name':'garlic salt',
+                            'quantity': '1',
+                            'unit':'tsp'
+                        },
+                        {
+                            'name':'parmesan cheese (optional)',
+                            'quantity': '',
+                            'unit':''
                         }
                     ],
                     'steps':[
                         {
-                            'text' : 'testy',
+                            'text' : 'Preheat oven to 450 degrees.',
                             'number': '1'
-                        }
+                        },
+                        {
+                            'text' : 'Stir broccoli, balsamic vinegar, olive oil, salt, pepper, and garlic salt in a large bowl until thoroughly combined.',
+                            'number': '2'
+                        },
+                        {
+                            'text' : 'Spread broccoli onto a large baking sheet.',
+                            'number': '3'
+                        },
+                        {
+                            'text' : 'Roast for 20 minutes or until edges begin to singe and blacken, stirring once on pan halfway through.',
+                            'number': '4'
+                        },
+                        {
+                            'text' : 'When broccoli is done transfer to bowl, drizzle with additional balsamic vinegar, and sprinkle with a bit of parmesan cheese if desired.',
+                            'number': '5'
+                        },
                     ],
-                    'image':'link/to/image'
+                    'image':'/images/balsamic_broccoli.png'
                 },
                 {
                     'title': 'Clove Garlic Soup',
                     'ingredients':[
                         {
-                            'name':'ing 1',
+                            'name':'garlic (halved crosswise)',
                             'quantity': '2',
-                            'unit':'oz'
-                        }
+                            'unit':'heads'
+                        },
+                        {
+                            'name':'extra-virgin olive oil',
+                            'quantity': '1',
+                            'unit':'tbsp'
+                        },
+                        {
+                            'name':'low sodium chicken stock',
+                            'quantity': '4',
+                            'unit':'cups'
+                        },
+                        {
+                            'name':'yukon gold potatoes',
+                            'quantity': '8',
+                            'unit':'ozs'
+                        },
+                        {
+                            'name':'freshly grated parmesan',
+                            'quantity': '1/4',
+                            'unit':'cup'
+                        },
+                        {
+                            'name':'freshly ground pepper',
+                            'quantity': '',
+                            'unit':''
+                        },
+                        {
+                            'name':'coarse salt',
+                            'quantity': '',
+                            'unit':''
+                        },
                     ],
                     'steps':[
                         {
-                            'text' : 'testy',
+                            'text' : 'Heat oven to 375 degrees. Drizzle garlic heads with oil. Wrap tightly in foil and roast until tender, about 40 minutes. Let cool, then squeeze garlic from papery skin and set aside.',
                             'number': '1'
+                        },
+                        {
+                            'text' : 'Bring stock, potatoes, and roasted garlic to a boil; reduce heat and simmer until potatoes are tender, about 12 minutes. Remove from heat and stir in Parmesan.',
+                            'number': '2'
+                        },
+                        {
+                            'text': 'Let cool slightly, then puree in a blender until smooth. Season with salt and pepper. Sprinkle with Parmesan and serve with rustic bread.',
+                            'number': '3'
                         }
                     ],
-                    'image':'link/to/image'
+                    'image':'/images/clove_garlic_soup.png'
                 },
                 {
                     'title': 'Crockpot BBQ Beef Sandwich',
                     'ingredients':[
                         {
-                            'name':'ing 1',
-                            'quantity': '2',
-                            'unit':'oz'
+                            'name':'chuck roast',
+                            'quantity': '3',
+                            'unit':'lbs'
+                        },
+                        {
+                            'name':'seasoning salt',
+                            'quantity': '',
+                            'unit':''
+                        },
+                        {
+                            'name':'pepper',
+                            'quantity': '',
+                            'unit':''
+                        },
+                        {
+                            'name':'paprika',
+                            'quantity': '',
+                            'unit':''
+                        },
+                        {
+                            'name':'garlic powder',
+                            'quantity': '',
+                            'unit':''
+                        },
+                        {
+                            'name':'bbq sauce',
+                            'quantity': '18',
+                            'unit':'ozs'
+                        },
+                        {
+                            'name':'beer (stout recommended)',
+                            'quantity': '12',
+                            'unit':'ozs'
                         }
                     ],
                     'steps':[
                         {
-                            'text' : 'testy',
+                            'text' : 'Sprinkle both sides of the roast with seasoned salt, pepper, paprika, and garlic powder.',
                             'number': '1'
-                        }
+                        },
+                        {
+                            'text' : 'Put the roast in your crockpot and pour on the bbq sauce.',
+                            'number': '2'
+                        },
+                        {
+                            'text' : 'Pour the beer over the top, and cook for 6-7 hours.',
+                            'number': '3.'
+                        },
+                        {
+                            'text' : 'Remove the meat and shred - it should be really tender and falling apart - and discard any fat.',
+                            'number': '4'
+                        },
+                        {
+                            'text' : 'Pour the cooking liquid into a large measuring cup and skim off the fat.',
+                            'number': '5'
+                        },
+                        {
+                            'text' : 'Mix as much of the liquid back into the meat as you want.',
+                            'number': '6'
+                        },
+                        {
+                            'text' : 'Serve immediately, or place beef back in the crockpot on warm until ready to serve.',
+                            'number': '7'
+                        },
                     ],
-                    'image':'/images/broccoli.png'
+                    'image':'/images/crockpot_bbq_beef.png'
                 }
             ],
             1:[
